f80175db2fcbcab4d6c460167e26193c6eb88b5b
laub <lauriborra@gmail.com>
2019-09-16 17:22:13 -0300

    octavo intento

--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, TextField, SubmitField, TextAreaField, PasswordField
+from wtforms import Form, StringField, TextField, SubmitField, TextAreaField, PasswordField
 from wtforms.validators import DataRequired, ValidationError, Email
 
 
 class ContactForm(FlaskForm):
-    name = TextField('Nombre', validators=[DataRequired(message= "Please enter your name")])
+    name = StringField('Nombre', validators=[DataRequired(message= "Please enter your name")])
     email = TextField('Email', validators=[DataRequired(message= "Please enter your email")])
     subject = TextField('Asunto', validators=[DataRequired(message= "Please enter your Asunto")])
     message = TextAreaField('Mensaje', validators=[DataRequired(message= "Please enter your mensaje")])
